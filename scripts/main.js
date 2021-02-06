07aee8ebbb7119147d5025a183ffa95639df53c4
H0rn0chse <h0rn0chse@gmx.de>
2021-02-06 14:54:03 +0100

    Added gabbing cursor during pan

--- scripts/main.js
+++ scripts/main.js
@@ -12,5 +12,33 @@ export function init() {
     })
 
     var grid = new Muuri('.grid')
+
+    addHandler()
+}
+
+function addHandler() {
+    document.addEventListener("keydown", evt => {
+        if (evt.ctrlKey && !globalThis.grabbing) {
+            document.body.style.cursor = "grab"
+        }
+    })
+    document.addEventListener("keyup", evt => {
+        if (!evt.ctrlKey && !globalThis.grabbing) {
+            document.body.style.cursor = ""
+        }
+    })
+    document.addEventListener("mousedown", evt => {
+        if (evt.ctrlKey) {
+            globalThis.grabbing = true
+            document.body.style.cursor = "grabbing"
+        }
+    })
+    document.addEventListener("mouseup", evt => {
+        globalThis.grabbing = false
+        if (evt.ctrlKey) {
+            document.body.style.cursor = "grab"
+        } else {
+            document.body.style.cursor = ""
+        }
     })
 }
