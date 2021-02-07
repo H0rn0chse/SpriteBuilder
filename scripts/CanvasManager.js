8b7511b14499a5e66bdf0781cbf1c4c5b7a24691
H0rn0chse <h0rn0chse@gmx.de>
2021-02-07 19:42:40 +0100

    Added CanvasManager

---
+++ scripts/CanvasManager.js
@@ -0,0 +1,29 @@
+class _CanvasManager {
+    constructor () {
+        this.canvas = null
+        this.ctx = null
+    }
+
+    init (element) {
+        this.canvas = element
+        this.ctx = this.canvas.getContext('2d');
+        this.setSize(200, 100)
+    }
+
+    reset () {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    setSize (width, height) {
+        this.canvas.width = width
+        this.canvas.height = height
+    }
+
+    drawImages (imageMap) {
+        imageMap.forEach((pos, img) => {
+            this.ctx.drawImage(img, pos.left, pos.top);
+        });
+    }
+}
+
+export const CanvasManager = new _CanvasManager()
